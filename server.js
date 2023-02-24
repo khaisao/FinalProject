c223d56f8ad42f5a97a41d62c468fd72a550644a
PVKhai <phamvankhai020601@gmail.com>
2023-02-25 01:44:07 +0700

    ưaeg

--- server.js
+++ server.js
@@ -1,32 +1,52 @@
-const express = require("express");
-const http = require("http");
-const socket = require("socket.io");
-const QRCode = require("qrcode");
+const net = require('net');
+const sharp = require('sharp');
+const fs = require('fs');
 
-const app = express();
-const port = process.env.PORT || 3000;
-const server = app.listen(port)
-
-const io = socket(server);
-app.use(express.static(__dirname));
+let data = Buffer.alloc(0);
 
 
-// app.get("/", (req, res) => {
-//     res.sendFile(__dirname + "/index.html");
-// });
+const server = net.createServer((socket) => {
+    console.log('Client connected');
 
-io.on("connection", (socket) => {
-    const ipAddress = socket.handshake.address;
-    const url = `ws://${ipAddress}:${port}`;
-    console.log("new connection" + socket.id)
-    socket.on("image", () => {
-        console.log('Nhan du lieu')
-        // console.log(data.image); // in ra chuỗi base64 của ảnh được gửi từ client
+    socket.on('data', function(chunk) {
+        // Append the new chunk to the existing data
+        data = Buffer.concat([data, chunk]);
+
+        // Check if the null terminator is present in the new chunk
+        const nullTerminatorIndex = data.indexOf(0x00);
+        if (nullTerminatorIndex >= 0) {
+            // Extract the message from the buffer and remove the null terminator
+            const message = data.toString('utf8', 0, nullTerminatorIndex);
+
+            console.log(`Received message: ${message}`);
+
+            // Extract the base64-encoded image data from the message
+            const imageData = message.substring(message.indexOf(',') + 1);
+
+            // Decode the base64-encoded data
+            const decodedData = Buffer.from(imageData, 'base64');
+
+            // Write the decoded data to a file
+            fs.writeFile('image.jpg', decodedData, function(err) {
+                if (err) throw err;
+                console.log('Image saved successfully');
+            });
+
+            // Remove the processed message from the data buffer
+            data = data.slice(nullTerminatorIndex + 1);
+        }
     });
-    QRCode.toDataURL(url, (err, src) => {
-        if (err) return console.error(err);
-        socket.emit("qr code", src);
+
+    socket.on('end', () => {
+        // console.log(`Received data: ${buffer}`);
+        console.log('Client disconnected');
     });
 });
 
+server.on('error', (err) => {
+    throw err;
+});
 
+server.listen(8080, () => {
+    console.log('Server listening on port 8080');
+});
