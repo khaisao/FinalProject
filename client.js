5a99f5f20216e51834d7464f91e80af03a8bf117
PVKhai <phamvankhai020601@gmail.com>
2023-02-26 19:37:15 +0700

    Optimize code

--- client.js
+++
@@ -1,9 +0,0 @@
-const socket = io();
-socket.on("connect", () => {
-    console.log("Connected to server");
-});
-
-socket.on("qr code", (src) => {
-    const qrcode = document.getElementById("qrcode");
-    qrcode.innerHTML = `<img src="${src}" alt="QR Code" />`;
-});
